a8d830b80109d3c24657f7ccc5b41249099b27b6
aditya <mrpareekaditya@gmail.com>
2024-06-27 11:44:15 +0530

    rem comm

--- server/models/RatingandReview.js
+++ server/models/RatingandReview.js
@@ -1,27 +1,26 @@
 const mongoose = require("mongoose");
 
-// Define the RatingAndReview schema
 const ratingAndReviewSchema = new mongoose.Schema({
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
 		required: true,
-		ref: "user",
+		ref: "user"
 	},
 	rating: {
 		type: Number,
-		required: true,
+		required: true
 	},
 	review: {
 		type: String,
 		required: true,
+		trim: true
 	},
 	course: {
 		type: mongoose.Schema.Types.ObjectId,
 		required: true,
 		ref: "Course",
-		index: true,
+		index: true
 	},
 });
 
-// Export the RatingAndReview model
 module.exports = mongoose.model("RatingAndReview", ratingAndReviewSchema);
