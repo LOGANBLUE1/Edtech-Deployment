17f986e73eb9c58790f5017808352a77faefee72
LOGANBLUE1 <mrpareekaditya@gmail.com>
2024-08-12 23:52:02 +0530

    Changed custom logger

--- src/services/log.js
+++ src/services/log.js
@@ -1,4 +1,11 @@
-export const log = (message) => {
+export const log = (...args) => {
     const stack = new Error().stack.split('\n')[2].trim();
-    console.log(`\x1b[34m${stack}\x1b[0m: ${message}`);
-}
+    const message = args.map(arg => {
+        if (arg === undefined) {
+            return 'undefined';
+        }
+        return typeof arg === 'object' ? JSON.stringify(arg, null, 2) : arg;
+    }).join(' ');
+
+    console.log(`\x1b[32m${stack}\x1b[0m: ${message}`);
+};
