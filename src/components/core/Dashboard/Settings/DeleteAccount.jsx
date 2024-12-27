6f68e57995e6618de1aca6d8a119c5911a6ebe58
LOGANBLUE1 <mrpareekaditya@gmail.com>
2024-12-27 11:26:41 +0530

    added delete icon control

--- src/components/core/Dashboard/Settings/DeleteAccount.jsx
+++ src/components/core/Dashboard/Settings/DeleteAccount.jsx
@@ -19,11 +19,24 @@ export default function DeleteAccount() {
     }
   }
 
+  const handleOpenConfirmationModal = () => {
+    setConfirmationModal({
+      text1: "Are you sure?",
+      text2: "Your account will be deleted forever.",
+      btn1Text: "Delete",
+      btn2Text: "Cancel",
+      btn1Handler: () => handleDeleteAccount(),
+      btn2Handler: () => setConfirmationModal(null),
+    });
+  };
+  
   return (
     <>
       <div className="my-10 flex flex-row gap-x-5 rounded-md border-[1px] border-pink-700 bg-pink-900 p-8 px-12">
-        <div className="flex aspect-square h-14 w-14 items-center justify-center rounded-full bg-pink-700">
-          <FiTrash2 className="text-3xl text-pink-200" />
+        <div className="flex aspect-square h-14 w-14 items-center justify-center rounded-full bg-pink-700 cursor-pointer"
+          onClick={() => handleOpenConfirmationModal()}
+        >
+          <FiTrash2 className="text-3xl text-pink-200"/>
         </div>
         <div className="flex flex-col space-y-2">
           <h2 className="text-lg font-semibold text-richblack-5">
@@ -39,16 +52,7 @@ export default function DeleteAccount() {
           <button
             type="button"
             className="w-fit cursor-pointer italic text-pink-300"
-            onClick={() =>
-              setConfirmationModal({
-                text1: "Are you sure?",
-                text2: "You're account will be deleted forever.",
-                btn1Text: "Delete",
-                btn2Text: "Cancel",
-                btn1Handler: () => handleDeleteAccount(),
-                btn2Handler: () => setConfirmationModal(null),
-              })
-            }
+            onClick={() => handleOpenConfirmationModal()}
           >
             I want to delete my account.
           </button>
