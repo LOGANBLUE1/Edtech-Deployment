c6d7202496d0c34ace2ad0f3fc54f34436cd9e7a
LOGANBLUE1 <mrpareekaditya@gmail.com>
2025-02-06 08:55:47 +0530

    cart error fixed

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -164,15 +164,6 @@ function CourseDetails() {
               </div>
             </div>
 
-            {/* <div className="flex w-full flex-col gap-4 border-y border-y-richblack-500 py-4 lg:hidden">
-              <p className="space-x-3 pb-4 text-3xl font-semibold text-richblack-5">
-                Rs. {price}
-              </p>
-              <button className="yellowButton" onClick={handleBuyCourse}>
-                Buy Nowsssssssssssssss
-              </button>
-              <button className="blackButton">Add to Cart</button>
-            </div> */}
           </div>
           {/* Courses Card */}
           <div className="right-[1rem] top-[60px] mx-auto hidden min-h-[600px] w-1/3 max-w-[410px] translate-y-24 md:translate-y-0 lg:absolute  lg:block">
